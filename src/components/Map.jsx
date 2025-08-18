9cddaaeb90df3441f3ae6972fbee9a7f56bbcf78
Saheli <engr.saheliroy@gmail.com>
2025-08-18 19:23:33 +0530

    When user clicks anywhere on Map, the Sidebar displays a form.The form has a back button functionality. Both case uses useNavigate hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,15 @@
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Map() {
+    const navigate = useNavigate();
+
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get('lat');
     const lng = searchParams.get('lng');
 
     console.log(setSearchParams);
-    return <div className={styles.mapContainer}>
+    return <div className={styles.mapContainer} onClick={() => navigate("form")}>
         <h1>Position: {lat} & {lng} </h1>
         <button onClick={() => setSearchParams({lat: 23, lng: 50})}>Change position</button>
     </div>
