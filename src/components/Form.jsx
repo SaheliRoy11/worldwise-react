04ec2e1d3d6c98444d96479942d9b4d7737ef954
Saheli <engr.saheliroy@gmail.com>
2025-09-02 23:49:19 +0530

    Create functionality for adding new City to our API and display updated list of cities if the new city is added successfully

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,8 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { useUrlPosition } from "../hooks/useUrlPosition";
 
 import styles from "./Form.module.css";
@@ -8,6 +10,8 @@ import Button from "./Button";
 import BackButton from "./BackButton";
 import Message from "./Message";
 import Spinner from "./Spinner";
+import {useCities} from "../contexts/CitiesContext";
+import { useNavigate } from "react-router-dom";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -28,28 +32,33 @@ function Form() {
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [emoji, setEmoji] = useState("");
   const [geocodingError, setGeocodingError] = useState("");
+  const {createCity, isLoading} = useCities();
+  const navigate = useNavigate();
 
   //fetch data when the component mounts
   useEffect(
     function () {
-
       //If the user directly manually enters url and goes to '/app/form' without any lat and lng values then return without making any http request
-      if(!lat && !lng) return;
+      if (!lat && !lng) return;
 
       async function fetchCityData() {
         try {
           setIsLoadingGeocoding(true);
-          setGeocodingError("");//reset if there was any error in the previous fetch request
+          setGeocodingError(""); //reset if there was any error in the previous fetch request
 
-          const response = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+          const response = await fetch(
+            `${BASE_URL}?latitude=${lat}&longitude=${lng}`
+          );
           const data = await response.json();
 
           //Handle the situation where a user might click on the waters or anywhere where there is no country on the map.
-          if(!data.countryCode) {
-            throw new Error("That doesn't seem to be a city.Click somewhere else 😊");
+          if (!data.countryCode) {
+            throw new Error(
+              "That doesn't seem to be a city.Click somewhere else 😊"
+            );
           }
 
-          setCityName(data.city || data.locality || "");//if user clicks on a small place the city might not be present, in that case use the locality, if that is also not given then use an empty string.
+          setCityName(data.city || data.locality || ""); //if user clicks on a small place the city might not be present, in that case use the locality, if that is also not given then use an empty string.
           setCountry(data.countryName);
           setEmoji(convertToEmoji(data.countryCode));
         } catch (error) {
@@ -62,6 +71,27 @@ function Form() {
     },
     [lat, lng]
   );
+
+
+  //The form will get submitted when one of the buttons is clicked, that is also the reason why in BackButton component's onClick event handler we did e.preventDefault() so that the form does not get submitted. This means only the Add button can submit the form now, but we dont want that as it will make the page hard reload and that should not happen in a SPA (Single Page Application).
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+      //id will be automatically created and added by the json-server
+    };  
+    
+    await createCity(newCity);
+    navigate('/app/cities');//display the updated list of cities after adding the new city
+  }
 
   if(isLoadingGeocoding) {
     return <Spinner />
@@ -76,7 +106,7 @@ function Form() {
   }
 
   return (
-    <form className={styles.form}>
+    <form className={`${styles.form} ${isLoading? styles.loading : ''}`} onSubmit={handleSubmit}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -89,10 +119,11 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        <DatePicker
           id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          selected={date} //instead of the value prop we use selected prop to display the selected date in the DatePicker (refer to documentation)
+          onChange={(d) => setDate(d)}
+          dateFormat="dd/MM/yyyy"
         />
       </div>
 
