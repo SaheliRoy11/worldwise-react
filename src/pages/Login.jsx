2e5e51b1852585f122492dc9e17df4572b746603
Saheli <engr.saheliroy@gmail.com>
2025-09-03 21:52:22 +0530

    Add fake authentication: Implementing login and logout of user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,33 @@
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) {
+      login(email, password);
+    }
+    return;
+  }
+
+  useEffect(function () {
+    if (isAuthenticated) navigate("/app", {replace: true});//if the user clicks on the back button of the browser after logging in, the url with still be "/app/cities".This is because even though the browser will go to "/login", it will again get redirected to '/app/cities'. After a user successfully logs in, replacing the login page in the history stack prevents them from navigating back to it using the browser's back button.
+  }, [isAuthenticated, navigate]);
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +49,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
